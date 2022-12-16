264739231b53355c063327e7e5268b741d6a25ea
Pablo Hernandez <hpablo575@gmail.com>
2022-12-16 14:39:04 -0700

    imported scanner and declared it in main class. Next, declared the scannner input in the program. Followed this with a prompt for user

--- weatherChecker.java
+++ weatherChecker.java
@@ -1,6 +1,23 @@
+//first thing to do is to use the scanner to extract the information from the user 
+
+import java.util.Scanner;
+
 public class weatherChecker {
-    public static void main (String[] args){
-// write a program 
+    // write a program that takes a temp input and returns a decision on what to wear based on said weather 
+
+    public static void main (String[] args)
+    {
+        // declare scanner input 
+        Scanner input = new Scanner(System.in);
+        // first, give the user a question
+        System.out.println("Enter temperature");
+
+        int temperature = input.nextInt();
+
+
+
+
+
 
 
 
